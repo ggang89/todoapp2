60b8866491d56773a566c010705078fb672af357
ggang89 <ggang89@gmail.com>
2024-02-27 18:03:33 +0900

    추가된 list가 보여질 하위요소 만들어줌

--- src/App.js
+++ src/App.js
@@ -12,6 +12,18 @@ function App() {
         ></input>
         <button>추가 </button>
       </div>
+      <ul>
+        <li className="todoList">
+          <p className="listText">리액트 배우기</p>
+          <button className="btn">수정</button>
+          <button>삭제</button>
+        </li>
+        <li className="todoList">
+          <p className="listText">노마드 강의 듣기</p>
+          <button className="btn">수정</button>
+          <button>삭제</button>
+        </li>
+      </ul>
     </div>
   );
 }

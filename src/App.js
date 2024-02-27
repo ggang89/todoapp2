c3f7fe764dca7d955ace0199dc4c2d1ebd6773cc
ggang89 <ggang89@gmail.com>
2024-02-27 17:52:55 +0900

    input 창 만들어줌

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ function App() {
   return (
     <div className="wrap">
       <h1 className="tytle">To Do App (version.2)</h1>
+      <div className="inputBox">
+        <input
+          className="inputAdd"
+          type="text"
+          placeholder="할 일을 입력하세요"
+        ></input>
+        <button>추가 </button>
+      </div>
     </div>
   );
 }

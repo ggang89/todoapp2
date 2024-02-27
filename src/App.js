12045cea27d4f5e7e0fb58c8e00b1f984575ecd4
ggang89 <ggang89@gmail.com>
2024-02-27 18:33:09 +0900

    useState에 isEditing 객체 추가해줌

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import "./App.css";
 import React, { useState } from "react";
 
 function App() {
-  const [todo, setTodo] = useState({ todoTitle: "리액트배우기" });
+  const [todo, setTodo] = useState({ todoTitle: "리액트배우기",isEditing:false, });
   // 문자로 넣어준 기본값을 객체로 수정
+
+  //1.리스트 창에서 수정버튼을 누른다. 
+  //==>isEditing=true
+  //2.삭제버튼이 사라지고 텍스트 입력창이 생긴다
+  //==>input창이 생성된다.
+  //3.수정 버튼을 다시 누르면 내용이 저장되고 삭제버튼이 생긴다.
   return (
     <div className="wrap">
       <h1 className="tytle">To Do App (version.2)</h1>

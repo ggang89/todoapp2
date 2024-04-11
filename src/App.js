5566d1c3a3fa661d1d6e2672f01ee1d587746897
ggang89 <ggang89@gmail.com>
2024-04-11 16:52:14 +0900

    todo목록에 이벤트 연결함(수정버튼 클릭시 수정창 나오고 수정 후 수정버튼 누르면 다시 목록창으로 변경됨)

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import "./App.css";
 import React, { useState } from "react";
 
 function App() {
-  const [todo, setTodo] = useState({ todoTitle: "리액트배우기",isEditing:false, });
-  // 문자로 넣어준 기본값을 객체로 수정
+  const [todo, setTodo] = useState({
+    todoTitle: "리액트 공부",
+    isEditing: false,
+  });
 
-  //1.리스트 창에서 수정버튼을 누른다. 
-  //==>isEditing=true
-  //2.삭제버튼이 사라지고 텍스트 입력창이 생긴다
-  //==>input창이 생성된다.
-  //3.수정 버튼을 다시 누르면 내용이 저장되고 삭제버튼이 생긴다.
+  const edit = () => {
+    setTodo({ ...todo, isEditing: !todo.isEditing });
+  };
+  const handleText = (e) => {
+    setTodo({ ...todo, todoTitle: e.target.value });
+  };
   return (
     <div className="wrap">
-      <h1 className="tytle">To Do App (version.2)</h1>
+      <h1 className="title">To Do App (version.2)</h1>
+
       <div className="inputBox">
         <input
           className="inputAdd"
@@ -21,18 +25,41 @@ function App() {
         ></input>
         <button>추가 </button>
       </div>
+
       <ul>
         <li className="todoList">
-          <p className="listText">{todo.todoTitle}</p>
-          {/* 연결된 변수를 객체의 키값으로 변경해줌 */}
-          <button className="btn">수정</button>
-          <button>삭제</button>
+          {todo.isEditing ? (
+            <>
+              <input value={todo.todoTitle} onChange={handleText} />
+              <button onClick={edit}>수정</button>
+            </>
+          ) : (
+            <>
+              <p className="listText">{todo.todoTitle}</p>
+              <button className="btn" onClick={edit}>
+                수정
+              </button>
+              <button>삭제</button>
+            </>
+          )}
         </li>
-        <li className="todoList">
-          <p className="listText">노마드 강의 듣기</p>
-          <button className="btn">수정</button>
-          <button>삭제</button>
-        </li>
+
+        {/* <li className="todoList">
+          {todo.isEditing ? (
+            <>
+            <input/>
+            <button onClick={edit}>수정</button>
+            </>
+          ) : (
+            <>
+              <p className="listText">노마드 강의 듣기</p>
+              <button className="btn" onClick={edit}>
+                수정
+              </button>
+              <button>삭제</button>
+            </>
+          )}
+        </li> */}
         <li className="todoList">
           <input className="updateTodo" value={todo.todoTitle}></input>
         </li>
